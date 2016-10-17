d530963d7ea10a9933b9eb8bd6df5573a762bb14
Romain Canon <romain.hydrocanon@gmail.com>
2016-10-17 23:38:33 +0200

    [BUGFIX] Fix missing extension in JavaScript file path

--- Configuration/TypoScript/View/Foundation/Foundation5.ts
+++ Configuration/TypoScript/View/Foundation/Foundation5.ts
@@ -4,7 +4,7 @@
             jQuery = 1
         }
         includeJSFooter {
-            foundation = EXT:formz_example/Resources/Public/Contrib/Foundation/Foundation5/js/foundation.min
+            foundation = EXT:formz_example/Resources/Public/Contrib/Foundation/Foundation5/js/foundation.min.js
         }
         includeCSS {
             foundation = EXT:formz_example/Resources/Public/Contrib/Foundation/Foundation5/css/foundation.min.css
