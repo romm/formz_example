a312cafcc98d9cc52691819663bc97b1737f753e
Romain Canon <romain.hydrocanon@gmail.com>
2016-10-19 19:48:16 +0200

    [TASK] Delete conditions on Bootstrap/Foundation assets inclusion

--- ext_tables.php
+++ ext_tables.php
@@ -22,13 +22,13 @@ call_user_func(
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
             $extensionKey,
             'Configuration/TypoScript/Bootstrap/Bootstrap3',
-            '[Formz] Form example - Twitter Bootstrap 3 Theme'
+            '[Formz] Form example - Twitter Bootstrap 3 Assets'
         );
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
             $extensionKey,
             'Configuration/TypoScript/Foundation/Foundation5',
-            '[Formz] Form example - Foundation 5 Theme'
+            '[Formz] Form example - Foundation 5 Assets'
         );
 
         $defaultPluginKey = 'formzexample_example';
