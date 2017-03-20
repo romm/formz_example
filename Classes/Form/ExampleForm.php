f7dd3dd44492d173b8afd4fe12a2a2986a32ea47
Romain Canon <romain.hydrocanon@gmail.com>
2017-03-20 17:50:51 +0100

    [TASK] Change copyright

--- Classes/Form/ExampleForm.php
+++ Classes/Form/ExampleForm.php
@@ -1,7 +1,14 @@
 <?php
 /*
- * 2016
- * Romain CANON <romain.canon.ext@direct-energie.com>
+ * 2017 Romain CANON <romain.hydrocanon@gmail.com>
+ *
+ * This file is part of the TYPO3 FormZ project.
+ * It is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, either
+ * version 3 of the License, or any later version.
+ *
+ * For the full copyright and license information, see:
+ * http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 namespace Romm\FormzExample\Form;
